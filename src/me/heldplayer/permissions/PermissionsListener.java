a1fbceedf8d5de5c0677369eaf5115e802a81ea7
Heldplayer <mitchel.pyl@gmail.com>
2015-07-31 00:02:28 +0200

    Add creative and spectator mode permissions nodes

--- src/me/heldplayer/permissions/PermissionsListener.java
+++ src/me/heldplayer/permissions/PermissionsListener.java
@@ -1,6 +1,8 @@
 package me.heldplayer.permissions;
 
 import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -51,15 +53,45 @@ public class PermissionsListener implements Listener {
     @EventHandler(priority = EventPriority.LOWEST)
     public void onPlayerChangedWorld(PlayerChangedWorldEvent event) {
         this.main.initPermissions(event.getPlayer());
+        if (!this.checkGamemode(event.getPlayer(), event.getPlayer().getGameMode())) {
+            event.getPlayer().setGameMode(GameMode.SURVIVAL);
+        }
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onPlayerRespawn(PlayerRespawnEvent event) {
         this.main.initPermissions(event.getPlayer());
+        if (!this.checkGamemode(event.getPlayer(), event.getPlayer().getGameMode())) {
+            event.getPlayer().setGameMode(GameMode.SURVIVAL);
+        }
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onPlayerJoin(PlayerJoinEvent event) {
         this.main.initPermissions(event.getPlayer());
+        if (!this.checkGamemode(event.getPlayer(), event.getPlayer().getGameMode())) {
+            event.getPlayer().setGameMode(GameMode.SURVIVAL);
+        }
     }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void test(PlayerGameModeChangeEvent event) {
+        if (!this.checkGamemode(event.getPlayer(), event.getNewGameMode())) {
+            event.setCancelled(true);
+            event.getPlayer().setGameMode(GameMode.SURVIVAL);
+        }
+    }
+
+    private boolean checkGamemode(Player player, GameMode gameMode) {
+        if (gameMode == GameMode.CREATIVE && !player.hasPermission("permissions.gamemode.creative")) {
+            player.sendMessage(ChatColor.RED + "You do not have permissions to be in creative mode here!");
+            return false;
+        }
+        if (gameMode == GameMode.SPECTATOR && !player.hasPermission("permissions.gamemode.spectator")) {
+            player.sendMessage(ChatColor.RED + "You do not have permissions to be in spectator mode here!");
+            return false;
+        }
+        return true;
+    }
+
 }
